e362c4254d4b9eb9d8a871092d2c50fbe3311082
kur0yama <41464808+kur0yama@users.noreply.github.com>
2020-07-14 06:56:00 +0800

    Precoded Il2Cpp Headers

---
+++ Precode/Il2Cpp/Il2Cpp.h
@@ -0,0 +1,50 @@
+//
+// Created by aimar on 12/28/2019.
+//
+#pragma once
+
+#include <stdio.h>
+#include <android/log.h>
+#include <dlfcn.h>
+#include <cstring>
+#include <string>
+#include <vector>
+#include <map>
+#include <inttypes.h>
+#include <codecvt>
+#include <locale>
+
+using namespace std;
+
+#ifndef ML_IL2CPP_H
+#define ML_IL2CPP_H
+
+#include "Il2CppType.h"
+
+namespace Il2Cpp {
+    int Attach(const char *libname = "libil2cpp.so");
+
+    void *GetImage(const char *image);
+
+    void *GetClass(const char *image, const char *namespaze, const char *clazz);
+
+    void *CreateArray(const char *image, const char *namespaze, const char *clazz, size_t length);
+
+    void *GetMethodOffset(const char *image, const char *namespaze, const char *clazz, const char *name, int argsCount = 0);
+
+    void *GetMethodOffset(const char *image, const char *namespaze, const char *clazz, const char *name, char **args, int argsCount = 0);
+
+    uintptr_t GetFieldOffset(const char *image, const char *namespaze, const char *clazz, const char *name);
+
+    void GetStaticFieldValue(const char *image, const char *namespaze, const char *clazz, const char *name, void *output);
+
+    void SetStaticFieldValue(const char *image, const char *namespaze, const char *clazz, const char *name, void *value);
+
+    void *NewClassObject(const char *image, const char *namespaze, const char *clazz);
+
+    bool IsAssembliesLoaded();
+};
+
+uintptr_t Il2CppBase();
+
+#endif //ML_IL2CPP_H
